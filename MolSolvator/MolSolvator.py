92a6b6285e70ff6af0097a6255949d53261280b7
gpantel <gpantelopulos@gmail.com>
2020-08-01 04:00:44 -0400

    Now with random 3D rotations~

--- MolSolvator/MolSolvator.py
+++ MolSolvator/MolSolvator.py
@@ -2,12 +2,17 @@ import MDAnalysis as mda
 import numpy as np
 from scipy.spatial import distance
 import toml 
+import argparse
 
-inputs = toml.load('input.toml')
+parser = argparse.ArgumentParser(description='Read in the TOML-format input file')
+parser.add_argument("-i", "--input", type=str, help='TOML-format input file')
+args = parser.parse_args()
+
+inputs = toml.load(args.input)
 
 if 'solute' not in inputs: raise Exception('solute missing from input file')
 if 'output' not in inputs: raise Exception('output missing from input file')
-if 'max_iterations' not in inputs: raise Exception('max_iterations missing from input file')
+
 if 'shape' not in inputs['solute_lattice']: raise Exception('shape of solute_lattice missing from input file')
 if 'width' not in inputs['solute_lattice']: raise Exception('width of solute_lattice missing from input file')
 if 'height' not in inputs['solute_lattice']: raise Exception('height of solute_lattice missing from input file')
@@ -18,6 +23,8 @@ if 'lower_z_position' not in inputs['solvent_lattice']: raise Exception('lower_z
 if 'upper_z_position' not in inputs['solvent_lattice']: raise Exception('upper_z_position of solvent_lattice missing from input file')
 if 'paths' not in inputs['solvent_molecules']: raise Exception('paths list of solvent_molecules missing from input file')
 if 'numbers' not in inputs['solvent_molecules']: raise Exception('numbers list of solvent_molecules missing from input file')
+if 'max_iterations' not in inputs['solvent_molecules']: raise Exception('max_iterations missing from input file')
+if 'rotate' not in inputs['solvent_molecules']: raise Exception('rotate missing from input file')
 if len(inputs['solvent_molecules']['paths']) != len(inputs['solvent_molecules']['numbers']):
     raise Exception('There should be an equal number of entries for paths and numbers of solent_molecules')
 
@@ -62,6 +69,26 @@ for i in range(len(solvent_lattice_sites[0])):
     if np.amin(distance.cdist(layers_positions, [xyz])) < solvent_lattice_buffer:
         solvent_lattice[solvent_lattice_sites[0][i]][solvent_lattice_sites[1][i]][solvent_lattice_sites[2][i]] = -1000
 
+def rand_rotation_matrix():
+    # This is a pythonic implementation of the random 3D rotation method described
+    # in the Graphics Gems book series by Jim Arvo in 1991
+    # http://www.realtimerendering.com/resources/GraphicsGems/gemsiii/rand_rotation.c
+    randnums = np.random.uniform(size=(3,))
+    theta, phi, z = randnums
+    theta = theta*2.0*np.pi  # Rotation about the pole (Z).
+    phi   = phi*2.0*np.pi  # For direction of pole deflection.
+    z     = z*2.0  # For magnitude of pole deflection.
+
+    r = np.sqrt(z)
+    V = (np.sin(phi) * r,
+         np.cos(phi) * r,
+         np.sqrt(2.0 - z))
+    st = np.sin(theta)
+    ct = np.cos(theta)
+    R = np.array(((ct, st, 0), (-st, ct, 0), (0, 0, 1)))
+    M = (np.outer(V, V) - np.eye(3)).dot(R)
+    return M
+
 def create_solvent_universe(path, solvent_molecule_id, n_residues):
     solvu = mda.Universe(path)
     solvuxyz = np.copy(solvu.atoms.positions)
@@ -89,7 +116,7 @@ def lookup_lattice_xyz(lattice_xyz, lattice_sites, site_index):
     x, y, z = lattice_xyz[lattice_sites[0][site_index]][lattice_sites[1][site_index]][lattice_sites[2][site_index]]
     return x, y, z
 
-def assign_lattice_positions(path, solvent_lattice, solvent_molecule_id):
+def assign_lattice_positions(path, solvent_lattice, solvent_molecule_id, rotation_matrices):
     solvu = mda.Universe(path)
     solvxyz = solvu.atoms.positions
     solvcom = solvu.atoms.center_of_mass()
@@ -99,6 +126,9 @@ def assign_lattice_positions(path, solvent_lattice, solvent_molecule_id):
     solvent_coordinates = []
     for i in range(len(solvent_lattice_sites[0])):
         xyz = np.copy(solvxyz - solvcom) # centered coordinates
+        if inputs['solvent_molecules']['rotate'] == True:
+            xyz = np.dot(xyz, rotation_matrices[i])
+
         x, y, z = lookup_lattice_xyz(solvent_lattice_xyz_coordinates, solvent_lattice_sites, i)
         xyz[:,0] += x
         xyz[:,1] += y
@@ -128,11 +158,11 @@ def insert_to_random_empty_point2(path, solvent_lattice, solvent_molecule_id):
     solvxyz = solvu.atoms.positions
     solvcom = solvu.atoms.center_of_mass()
     xyz = np.copy(solvxyz - solvcom) # centered coordinates
+    if inputs['solvent_molecules']['rotate'] == True:
+        rotation_matrix = rand_rotation_matrix()
+        xyz = np.dot(xyz, rand_rotation_matrix())
+    else: rotation_matrix = 0
 
-    # find +, - axis lengths of grid around solvent
-    # positive x
-    # @TODO this floor division is far too conservative -- molecules can nearly be on top of each other now
-    # need to add some kind of "buffer" distance
     n_pos_x =  int(((abs(max(xyz[:,0]))+solvent_lattice_buffer) // solvent_lattice_spacing))
     n_neg_x = int(-((abs(min(xyz[:,0]))+solvent_lattice_buffer) // solvent_lattice_spacing))
     n_pos_y =  int(((abs(max(xyz[:,1]))+solvent_lattice_buffer) // solvent_lattice_spacing))
@@ -142,8 +172,6 @@ def insert_to_random_empty_point2(path, solvent_lattice, solvent_molecule_id):
     n_range_x = list(range(n_neg_x, n_pos_x+1))
     n_range_y = list(range(n_neg_y, n_pos_y+1))
     n_range_z = list(range(n_neg_z, n_pos_z+1))
-#    if solvent_molecule_id == 1: print(xyz)
-#    if solvent_molecule_id == 1: print(n_range_x, n_range_y, n_range_z)
 
     empty_lattice_sites = np.array(np.where(solvent_lattice == 0))
     # prune empty lattice sites that can't fit the excess space
@@ -164,7 +192,7 @@ def insert_to_random_empty_point2(path, solvent_lattice, solvent_molecule_id):
         randcol, randrow, randdep = assign_empty_lattice_site(empty_lattice_sites)
         # check surrounding cells to ensure they are also empty. If not, re-roll
         iteration = 0
-        while iteration < inputs['max_iterations']:
+        while iteration < inputs['solvent_molecules']['max_iterations']:
             iteration += 1
             choice_boolean = check_nearby_lattice_sites(n_range_x, n_range_y, n_range_z, randcol, randrow, randdep, solvent_lattice)
             if choice_boolean == True:
@@ -178,11 +206,10 @@ def insert_to_random_empty_point2(path, solvent_lattice, solvent_molecule_id):
                 break
             elif choice_boolean == False:
                 randcol, randrow, randdep = assign_empty_lattice_site(empty_lattice_sites)
-        if iteration == inputs['max_iterations']:
+        if iteration == inputs['solvent_molecules']['max_iterations']:
             raise Exception('Lattice full!')
-        return solvent_lattice
+        return solvent_lattice, rotation_matrix
 
-# empty list to hold groups of solvents in order
 solvent_groups = []
 for i in range(len(inputs['solvent_molecules']['paths'])):
     solvent_molecule_id = i+1
@@ -190,15 +217,14 @@ for i in range(len(inputs['solvent_molecules']['paths'])):
     num_solvent = inputs['solvent_molecules']['numbers'][i]
     print('Inserting %i molecules of '%(num_solvent) + path)
     solvent_universe = create_solvent_universe(path, solvent_molecule_id, num_solvent)
+    rotation_matrices = []
     for i in range(num_solvent):
-        solvent_lattice = insert_to_random_empty_point2(path, solvent_lattice, solvent_molecule_id)
+        solvent_lattice, rotation_matrix = insert_to_random_empty_point2(path, solvent_lattice, solvent_molecule_id)
+        rotation_matrices.append(rand_rotation_matrix())
 
-    solvent_universe.atoms.positions = assign_lattice_positions(path, solvent_lattice, solvent_molecule_id)
+    solvent_universe.atoms.positions = assign_lattice_positions(path, solvent_lattice, solvent_molecule_id, rotation_matrices)
     solvent_groups.append(solvent_universe.atoms)
-    solvent_universe.atoms.write('solvent%i.pdb', bonds=None)
 
 all_solvent_universe = mda.Merge(*solvent_groups)
-all_solvent_universe.atoms.write('solvent_all.pdb', bonds=None)
-
 full_universe = mda.Merge(u.atoms, all_solvent_universe.atoms)
 full_universe.atoms.write(inputs['output'], bonds=None)
